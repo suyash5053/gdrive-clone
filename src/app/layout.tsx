7c13cbbb003535a5aaae78144882519cb35d1c28
Suyash Srivastava <suyashsrivastava5053@gmail.com>
2025-02-02 11:48:03 +0000

    making a basic ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
+    <html lang="en" className={`${GeistSans.variable}`} suppressHydrationWarning>
       <body>{children}</body>
     </html>
   );
